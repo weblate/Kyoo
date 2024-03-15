567a76ba88e104b56390c4ee3b4ecc7d22774629
Zoe Roux <zoe.roux@zoriya.dev>
2024-03-15 23:01:57 +0100

    Add a dummy echo websocket endpoint

---
+++ websockets/go.mod
@@ -0,0 +1,5 @@
+module github.com/zoriya/kyoo/websockets
+
+go 1.21.7
+
+require nhooyr.io/websocket v1.8.10 // direct
---
+++ websockets/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	websocket "nhooyr.io/websocket"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+			InsecureSkipVerify: true,
+		})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer c.Close(websocket.StatusInternalError, "Internal error")
+
+		for {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			defer cancel()
+
+			t, message, err := c.Read(ctx)
+			if err != nil || t != websocket.MessageText {
+				break
+			}
+
+			log.Printf("Received %v", message)
+
+			err = c.Write(ctx, websocket.MessageText, message)
+			if err != nil {
+				break
+			}
+		}
+	})
+
+	err := http.ListenAndServe(":7777", nil)
+	log.Fatal(err)
+}
